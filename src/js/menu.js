102ef07b5972a1e33b7851883383b757d731ec9a
Nikolai <nikolaj.dudin.@bk.ru>
2025-03-13 21:59:58 +0300

    not click to button while menu open

--- src/js/menu.js
+++ src/js/menu.js
@@ -2,6 +2,7 @@ $(document).ready(function() {
     $('#nav-icon2').click(function(){
         $(this).toggleClass('open');
         $('.nav_visible').toggleClass('open-menu');
+        $('.nav__block').toggleClass('nav__block__open');
         $('body').toggleClass('fixed-page');
         $('container').toggleClass('container-brightness');
     });
